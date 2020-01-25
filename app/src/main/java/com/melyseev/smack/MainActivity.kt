1947483e8c879d4d371cd26c7ca64ed244d95e51
Mikhail Eliseev <mishaeliseev1983@gmail.com>
2020-01-25 18:56:32 +0500

    lesson3

--- app/src/main/java/com/melyseev/smack/MainActivity.kt
+++ app/src/main/java/com/melyseev/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.melyseev.smack
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -49,7 +50,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginBtnNavClicked(view:View){
-
+        val loginIntent= Intent(this, LoginActivity::class.java)
+        startActivity( loginIntent )
     }
 
     fun addChannelClicked(view: View){
