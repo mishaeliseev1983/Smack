1c778dffd9b21b0686e17a0414e6f922d1214da4
Mikhail Eliseev <mishaeliseev1983@gmail.com>
2020-05-02 00:45:32 +0500

    login added progressbar

--- app/src/main/java/com/melyseev/smack/controller/LoginActivity.kt
+++ app/src/main/java/com/melyseev/smack/controller/LoginActivity.kt
@@ -1,9 +1,12 @@
 package com.melyseev.smack.controller
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.melyseev.smack.R
 import com.melyseev.smack.services.AuthService
 import kotlinx.android.synthetic.main.activity_login.*
@@ -13,21 +16,35 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        progressBarLogin.visibility = View.INVISIBLE
     }
 
 
     fun loginLoginBtnClicked(view: View){
         val email= txtLoginEmail.text.toString()
         val password = txtLoginPassword.text.toString()
-
-        AuthService.loginUser(this, email, password){
-            loginSuccess->if (loginSuccess){
-            AuthService.findUserByEmail(this){
-                findSuccess->if(findSuccess){
-                finish()
+        hideKeyBoard()
+        showLoading(true)
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            AuthService.loginUser(this, email, password) { loginSuccess ->
+                if (loginSuccess) {
+                    AuthService.findUserByEmail(this) { findSuccess ->
+                        if (findSuccess) {
+                            showLoading(false)
+                            finish()
+                        } else {
+                            //error find user
+                            showErrorToast()
+                        }
+                    }
+                } else {
+                    //error login
+                    showErrorToast()
+                }
             }
-            }
-        }
+        }else{
+            showErrorToast("Please fill in both email and password")
         }
     }
 
@@ -35,5 +52,28 @@ class LoginActivity : AppCompatActivity() {
         val intentCreateUser= Intent(this, CreateUserActivity::class.java)
         startActivity( intentCreateUser )
         finish()
+    }
+
+
+    fun showLoading(loading: Boolean){
+        if(loading){
+            progressBarLogin.visibility= View.VISIBLE
+        }else{
+            progressBarLogin.visibility= View.INVISIBLE
+        }
+        btnLoginCreateUser.isEnabled= !loading
+        btnLoginLogin.isEnabled= !loading
+    }
+
+    fun showErrorToast(errorMessage:String="Something went wrong"){
+        Toast.makeText(this,errorMessage, Toast.LENGTH_LONG).show()
+        showLoading( false )
+    }
+
+    fun hideKeyBoard(){
+        val inputManager= getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
     }
 }
